d48a3d9c2b2057c37ec570688dac88f1991ffa21
brian villarroel <villarroelbrian1110@gmail.com>
2022-01-10 15:45:37 -0300

    se agragan funcinalidades

--- web/src/components/navBar.jsx
+++ web/src/components/navBar.jsx
@@ -5,8 +5,17 @@ function NavBar() {
 
   return (
     <div className="navBar">
-      <input className='inputBuscar' type="text" placeholder='Buscar...'/>
-      <input className='inputBtnBuscar' type="button" value="Buscar" />
+      <div className='navBar_menu'>
+        <a href="#"></a>
+        <h1>E-Changuito</h1>
+      </div>
+      <div>
+        <input className='inputBuscar' type="text" placeholder='Buscar...'/>
+        <input className='inputBtnBuscar' type="button" value="Buscar" />
+      </div>
+      <div>
+        <a href="#">CHANGUITO</a>
+      </div>
     </div>
   );
 }
